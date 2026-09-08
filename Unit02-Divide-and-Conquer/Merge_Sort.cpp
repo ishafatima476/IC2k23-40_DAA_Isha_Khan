d6717723e9f6279a4bd1b57ffb88dcf06c676711
Isha Khan <ishafatima476@gmail.com>
2026-09-08 20:57:27 +0530

    Create Merge_Sort.cpp

---
+++ Unit02-Divide-and-Conquer/Merge_Sort.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high) {
+
+    int temp[100];
+
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= high) {
+
+        if (arr[i] < arr[j]) {
+            temp[k] = arr[i];
+            i++;
+        }
+        else {
+            temp[k] = arr[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i <= mid) {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high) {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (i = low, k = 0; i <= high; i++, k++) {
+        arr[i] = temp[k];
+    }
+}
+
+void mergeSort(int arr[], int low, int high) {
+
+    if (low >= high)
+        return;
+
+    int mid = (low + high) / 2;
+
+    mergeSort(arr, low, mid);
+
+    mergeSort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
+}
+
+int main() {
+
+    int n, arr[100];
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
